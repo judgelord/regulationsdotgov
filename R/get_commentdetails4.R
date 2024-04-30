6c2e92548418aac2cfd74bb3efe48975d5ac0036
Maya Khuzam <mayakhuzam@Mayas-MacBook-Pro-10.local>
2024-04-29 22:15:21 -0400

    added a delay to stop API limits

--- R/get_commentdetails4.R
+++ R/get_commentdetails4.R
@@ -6,29 +6,35 @@ library(jsonlite)
 library(tidyverse)
 library(magrittr)
 
-commentids <- d$id[1:1000]
-  
+commentids <- d$id[1:1010]
+
 get_commentdetails4 <- function(commentId,
-                                  lastModifiedDate = Sys.time(),
-                                  pages){
-    
-    path <- lapply(commentId, function(id) {
-      paste0("https://api.regulations.gov/v4/comments/", 
-             id,
-             "?include=attachments",
-             "&api_key=", api_key)
-    })
+                                lastModifiedDate = Sys.time(),
+                                pages,
+                                delay_seconds = 3.6) { 
   
-  result <- purrr::map(path, ~(GET(.x)))
+  path <- lapply(commentId, function(id) {
+    paste0("https://api.regulations.gov/v4/comments/", 
+           id,
+           "?include=attachments",
+           "&api_key=", api_key)
+  })
   
+  fetch_with_delay <- function(url) {
+    Sys.sleep(delay_seconds)  
+    GET(url)
+  }
+  
+  result <- purrr::map(path, ~slowly(fetch_with_delay, rate = rate_delay(delay_seconds))(.x))
+  
+
   metadata <- purrr::map(result, ~fromJSON(rawToChar(.x$content)))
   
-  first5k <- purrr::map_dfr(metadata, ~{
-    data <- .x$data$attributes
-    })
+
+  first5k <- purrr::map_dfr(metadata, ~.x$data$attributes)
   
   return(first5k)
-  }
+}
 
 attachment <- get_commentdetails4(commentId = "OMB-2023-0001-18154")
  
