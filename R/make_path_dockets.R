f991d1cfe5d678d67be49f925359991555a33ce6
Maya Khuzam <mayakhuzam@Mayas-MacBook-Pro-10.local>
2024-07-25 16:25:45 -0400

    Fixed: 1) agencyId filtering 2) lastModifiedDate was set to system time so it was not being altered in the larger order function

--- R/make_path_dockets.R
+++ R/make_path_dockets.R
@@ -1,13 +1,15 @@
-make_path_dockets <- function(agency, lastModifiedDate = Sys.time()){
+make_path_dockets <- function(agency, lastModifiedDate){
 
   paste0("https://api.regulations.gov",
          "/v4/",
          "dockets",
          "?",
-         "filter[agency]=", agency, "&",
+         "filter[agencyId]=", agency, "&",
          "filter[lastModifiedDate][le]=", lastModifiedDate, "&", #less than or equal to (vs [ge] in the api docs)
          "page[size]=250", "&",
          "page[number]=", 1:20, "&", #FIXME replace with 2 with 20 when done testing
          "sort=-lastModifiedDate,documentId", "&",
          "api_key=", api_key)
 }
+
+
