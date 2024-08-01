6dfb853da5e33e6af683286d3c2140febe345c9f
Maya Khuzam <mayakhuzam@Mayas-MacBook-Pro-10.local>
2024-08-01 16:40:39 -0400

    8 agencies have more than 5k dockets and the original get_dockets function was not sufficient for these cases
    #7

---
+++ R/get_dockets_batch.R
@@ -0,0 +1,63 @@
+v##############
+# REQUIRES HELPER FUNCTIONS #
+####################
+library(magrittr)
+library(stringr)
+library(lubridate)
+library(tidyverse)
+library(httr)
+source("R/make_path_dockets.R")
+source("R/make_dataframe.R")
+
+get_dockets_batch <- function(agency, 
+                        lastModifiedDate = Sys.time(), 
+                        api_keys){
+  
+  lastModifiedDate <- lastModifiedDate  %>%
+    ymd_hms() %>%
+    with_tz(tzone = "America/New_York") %>%
+    # replace spaces with unicode
+    gsub(" ", "%20", .) %>%
+    str_remove("\\..*")
+  
+  path <- make_path_dockets(agency, lastModifiedDate)
+  
+  # map GET function over pages
+  result <- purrr::map(path, GET)
+  
+  # report result status
+  status <<- map(result, status_code) |> tail(1) %>% as.numeric()
+  
+  url <- result[[20]][1]$url
+  
+  if(status != 200){
+    message(paste(Sys.time() |> format("%X"),
+                  "| Status", status,
+                  "| URL:", url))
+    
+    # small pause to give user a chance to cancel
+    Sys.sleep(6)
+  }
+  
+  # EXTRACT THE MOST RECENT x-ratelimit-remaining and pause if it is 0
+  remaining <<-  map(result, headers) |>
+    tail(1) |>
+    pluck(1, "x-ratelimit-remaining")
+  
+  if(remaining < 2){
+    
+    message(paste(Sys.time()|> format("%X"), "- Hit rate limit, will continue after one minute"))
+    
+    Sys.sleep(60)
+  }
+  
+  
+  # map the content of successful api results into a list
+  docket_metadata <- purrr::map_if(result, ~ status_code(.x) == 200, ~fromJSON(rawToChar(.x$content)))
+  
+  d <- map_dfr(docket_metadata, make_dataframe)
+  
+  
+  return(d)
+  
+}
