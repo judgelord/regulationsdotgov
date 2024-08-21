4b80b4e80da98a69d582f0a1df8dd0907741c45a
judgelord <devin.jl@gmail.com>
2024-08-21 14:20:58 -0400

    #15

--- R/helper_functions/format_date.R
+++ R/helper_functions/format_date.R
@@ -4,18 +4,28 @@
 
 
 format_date <- function(lastModifiedDate){
-  
+
   timezone <- base::format(lastModifiedDate, format="%Z")
-  
+
   # Convert to POSIXct
   date_posixct <- as.POSIXct(lastModifiedDate, format = "%Y-%m-%d %H:%M:%S", tz = "EDT")
-  
+
   # Convert to UTC
   date_utc <- format(date_posixct, tz = "UTC", usetz = FALSE)
-  
+
   # Format spaces
   formatted_date <- gsub(" ", "%20", date_utc)
-    
-  return(formatted_date)                   
+
+  return(formatted_date)
+}
+
+
+format_date2 <- function(lastModifiedDate){
+
+  formatted_date <- lastModifiedDate |>
+    str_replace("T", "%20") |>
+    str_remove_all("[A-Z]")
+
+  return(formatted_date)
 }
 
