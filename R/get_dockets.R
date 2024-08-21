fee2ac6de86810677f972b737d7ca4be2d773fdb
judgelord <devin.jl@gmail.com>
2024-08-21 14:28:15 -0400

    #7 #15

--- R/get_dockets.R
+++ R/get_dockets.R
@@ -9,34 +9,40 @@ library(dplyr)
 source("R/get_dockets_batch.R")
 
 
-get_dockets <- function(agency, 
-                        lastModifiedDate = Sys.time(), 
+get_dockets <- function(agency,
+                        lastModifiedDate = Sys.time(),
                         api_keys){
-  
+
+  lastModifiedDate <- format_date(lastModifiedDate)
+
+
   # Fetch the initial 5k and establish the base dataframe
-  metadata <- get_dockets_batch(agency, lastModifiedDate = Sys.time())
-  
+  metadata <- get_dockets_batch(agency, lastModifiedDate)
+
   # Loop until last page is TRUE
   while( !tail(metadata$lastpage, 1) | nrow(metadata) %% 5000 == 0 ) {
-    
+
     # Fetch the next batch of comments using the last modified date
+    lastModifiedDate = tail(metadata$lastModifiedDate,  n = 1) |>
+    format_date2()
+
     nextbatch <- get_dockets_batch(agency,
-                                   lastModifiedDate = tail(metadata$lastModifiedDate,
-                                                           n = 1)
-    )
+                                   lastModifiedDate
+                                   )
+
     message(paste(nrow(metadata), "+", nrow(nextbatch)))
-    
+
     # Append next batch to comments
     metadata <- suppressMessages(
       full_join(metadata, nextbatch)
     )
-    
+
     message(paste(" = ", nrow(metadata)))
-    
+
   }
-  
+
   return(metadata)
-  
+
 }
 
 # TESTING
