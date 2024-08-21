2707723da31bc4c70012971590211b0a17d70cd8
judgelord <devin.jl@gmail.com>
2024-08-21 14:27:33 -0400

    #7 as per #15, we need to format sys.time in the high level function, not in the batch function because we also want to call batches with the lastModifiedDate from returned metadata (so that we can get the next batch) --- this is working for the get_searchTerm functions if we need to copy

--- R/get_dockets_batch.R
+++ R/get_dockets_batch.R
@@ -10,50 +10,50 @@ source("R/helper_functions/make_path_dockets.R")
 source("R/helper_functions/make_dataframe.R")
 source("R/helper_functions/format_date.R")
 
-get_dockets_batch <- function(agency, 
-                        lastModifiedDate = Sys.time(), 
+get_dockets_batch <- function(agency,
+                        lastModifiedDate,
                         api_keys){
-  
-  lastModifiedDate <- format_date(lastModifiedDate) 
-  
+
+
   path <- make_path_dockets(agency, lastModifiedDate)
-  
+
   # map GET function over pages
   result <- purrr::map(path, GET)
-  
+
   # report result status
   status <<- map(result, status_code) |> tail(1) %>% as.numeric()
-  
+
   url <- result[[20]][1]$url
-  
+
   if(status != 200){
     message(paste(Sys.time() |> format("%X"),
                   "| Status", status,
                   "| URL:", url))
-    
+
     # small pause to give user a chance to cancel
     Sys.sleep(6)
   }
-  
+
   # EXTRACT THE MOST RECENT x-ratelimit-remaining and pause if it is 0
   remaining <<-  map(result, headers) |>
     tail(1) |>
-    pluck(1, "x-ratelimit-remaining")
-  
+    pluck(1, "x-ratelimit-remaining") |>
+    as.numeric()
+
   if(remaining < 2){
-    
-    message(paste(Sys.time()|> format("%X"), "- Hit rate limit, will continue after one minute"))
-    
+
+    message(paste("|", Sys.time()|> format("%X"), "| Hit rate limit, will continue after one minute |", remaining, "remaining"))
+
     Sys.sleep(60)
   }
-  
-  
+
+
   # map the content of successful api results into a list
   docket_metadata <- purrr::map_if(result, ~ status_code(.x) == 200, ~fromJSON(rawToChar(.x$content)))
-  
+
   d <- map_dfr(docket_metadata, make_dataframe)
-  
-  
+
+
   return(d)
-  
+
 }
