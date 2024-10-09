ef3385a316947695f577ff389dd4f52ee948e9b2
mayakhuzam2 <you@example.com>
2024-10-09 13:21:14 -0500

    #17 Fixed issue that wastes unneccesary API calls, the function now loops over the 20 paths and breaks once it reaches last page (doesn't use a while loop, I think this makes more sense)
    - still needs testing especially to make sure the error handling is working properly, not sure if some thing should be outside the loop or are fine inside

--- R/get_comments_batch.R
+++ R/get_comments_batch.R
@@ -14,59 +14,63 @@ get_comments_batch <- function(commentOnId,
 
   # call the make path function to make paths for the first 20 pages of 250 results each
   path <- make_path_commentOnId(commentOnId, lastModifiedDate, api_key)
-
-  # map GET function over pages
-  result <- purrr::map(path, GET)
-
-  # report result status
-  status <<- map(result, status_code) |> tail(1) %>% as.numeric()
-
-  url <- result[[20]][1]$url
-
-  if(status != 200){
-    message(paste(Sys.time() |> format("%X"),
-                  "| Status", status,
-                  "| URL:", url))
-
-    # small pause to give user a chance to cancel
-    Sys.sleep(6)
-  }
-
-  # EXTRACT THE MOST RECENT x-ratelimit-remaining and pause if it is 0
-  remaining <<-  map(result, headers) |>
-    tail(1) |>
-    pluck(1, "x-ratelimit-remaining") |>
-    as.numeric()
-
-  if(remaining < 2){
+  
+  metadata <- list()
+  
+  for (i in path){
+      
+    # map GET function over pages
+    result <- GET(i)
     
-    message(paste("|", Sys.time()|> format("%X"), "| Hit rate limit, will continue after one minute |", remaining, "remaining"))
+    # report result status
+    status <- result$status_code
+    url <- result$url
     
-    # ROTATE KEYS
-    api_keys <<- c(tail(api_keys, -1), head(api_keys, 1))
-    api_key <- api_keys[1]
-    #api_key <<- apikeys[runif(1, min=1, max=3.999) |> floor() ]
-    message(paste("Rotating api key to", api_key))
-    
-    Sys.sleep(60)
-  }
-
- # map the content of successful api results into a list
-  metadata <- purrr::map_if(result, ~ status_code(.x) == 200, ~fromJSON(rawToChar(.x$content)))
-
-  # print unsuccessful api calls (might be helpful to know which URLs are failing)
-
-  purrr::walk2(result,
-               path,
-               function(response, url) {
-    if (status_code(response) != 200) {
-      message(paste(status_code(response),
-                  "Failed URL:",
-                  url)
-            )
+    if(status != 200){
+      message(paste(Sys.time() |> format("%X"),
+                    "| Status", status,
+                    "| Failed URL:", url))
+      
+      # small pause to give user a chance to cancel
+      Sys.sleep(6)
     }
+    
+    ## print unsuccessful api calls (might be helpful to know which URLs are failing)
+    #I don't think we need this? Repetitive? 
+    #purrr::walk2(result,
+    #             path,
+    #             function(response, url) {
+    #               if (status_code(response) != 200) {
+    #                 message(paste(status_code(response),
+    #                               "Failed URL:",
+    #                               url)
+    #                 )
+    #               }
+    #             }
+    #)
+    
+    if(status == 200){
+      metadata[[i]] <- fromJSON(rawToChar(result$content))
+    }
+    
+    # EXTRACT THE MOST RECENT x-ratelimit-remaining and pause if it is 0
+    remaining <<- result$headers$`x-ratelimit-remaining` %>% as.numeric()
+    
+    if(remaining < 2){
+      
+      message(paste("|", Sys.time()|> format("%X"), "| Hit rate limit, will continue after one minute |", remaining, "remaining"))
+      
+      # ROTATE KEYS
+      api_keys <<- c(tail(api_keys, -1), head(api_keys, 1))
+      api_key <- api_keys[1]
+      #api_key <<- apikeys[runif(1, min=1, max=3.999) |> floor() ]
+      message(paste("Rotating api key to", api_key))
+      
+      Sys.sleep(60)
+    }
+    
+    if(metadata[[url]]$meta$lastPage == TRUE){break} # breaks loop when last page is reached
   }
-  )
 
   # map the list into a dataframe with the information we care about
   d <- map_dfr(metadata, make_dataframe)
@@ -77,85 +81,3 @@ get_comments_batch <- function(commentOnId,
   return(d)
 }
 
-#get_comments_batch <- function(commentOnId, # MAYA'S ATTEMPT AT REPLACING MAP WITH A WHILE LOOP, NOT WORKING 
-#                               lastModifiedDate = Sys.time(),
-#                               api_keys){
-#  
-#  api_key <- api_keys[1]
-#  
-#  lastModifiedDate <- format_date(lastModifiedDate)
-#  
-#  # call the make path function to make paths for the first 20 pages of 250 results each
-#  path <- make_path_commentOnId(commentOnId, lastModifiedDate, api_key)
-#  
-#  metadata <- list()
-#  lastPage <- NULL
-#  
-#  for (i in seq_along(path)) {
-#    
-#    while (is.null(lastPage) || !lastPage) {
-#      
-#      result <- GET(path[i])
-#      
-#      # report result status
-#      status <- result$status_code
-#      url <- result$url
-#      
-#      if(status != 200){
-#        message(paste(Sys.time() |> format("%X"),
-#                      "| Status", status,
-#                      "| URL:", url))
-#        
-#        # small pause to give user a chance to cancel
-#        Sys.sleep(6)
-#      }
-#      
-#      # EXTRACT THE MOST RECENT x-ratelimit-remaining and pause if it is 0
-#      remaining <<- result$headers$`x-ratelimit-remaining` %>% as.numeric()
-#      
-#      if(remaining < 2){
-#        
-#        message(paste("|", Sys.time()|> format("%X"), "| Hit rate limit, will continue after one minute |", remaining, "remaining"))
-#        
-#        # ROTATE KEYS
-#        api_keys <<- c(tail(api_keys, -1), head(api_keys, 1))
-#        api_key <- api_keys[1]
-#        #api_key <<- apikeys[runif(1, min=1, max=3.999) |> floor() ]
-#        message(paste("Rotating api key to", api_key))
-#        
-#        Sys.sleep(60)
-#      }
-#      
-#      if(status == 200){
-#        response_content <- fromJSON(rawToChar(result$content))
-#        metadata[[i]] <- response_content
-#        
-#        # Update lastPage
-#        lastPage <- pluck(response_content, "meta", "lastPage") 
-#      }
-#    }
-#  }
-#  
-#  # print unsuccessful api calls (might be helpful to know which URLs are failing)
-#  
-#  purrr::walk2(result,
-#               path,
-#               function(response, url) {
-#                 if (status_code(response) != 200) {
-#                   message(paste(status_code(response),
-#                                 "Failed URL:",
-#                                 url)
-#                   )
-#                 }
-#               }
-#  )
-#  
-#  # map the list into a dataframe with the information we care about
-#  d <- map_dfr(metadata, make_dataframe)
-#  
-#  # add back in the id for the document being commented on
-#  d$commentOnId <- commentOnId
-#  
-#  return(d)
-#}
-#
