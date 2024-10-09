5014f2404a8f65e631caba60db6ea57e35a08666
mayakhuzam2 <you@example.com>
2024-10-09 13:55:45 -0500

    #17 updated to reduce API hit waste, but not working as efficiently as it does for commentOnId ... still investigating why

--- R/get_dockets_batch.R
+++ R/get_dockets_batch.R
@@ -11,46 +11,49 @@ get_dockets_batch <- function(agency,
   lastModifiedDate <- format_date(lastModifiedDate)
 
   path <- make_path_dockets(agency, lastModifiedDate, api_key)
+  
+  metadata <- list()
 
-  # map GET function over pages
-  result <- purrr::map(path, GET)
+  for (i in path){
+    
+    # map GET function over pages
+    result <- GET(i)
 
-  # report result status
-  status <<- map(result, status_code) |> tail(1) %>% as.numeric()
-
-  url <- result[[20]][1]$url
-
-  if(status != 200){
-    message(paste(Sys.time() |> format("%X"),
-                  "| Status", status,
-                  "| URL:", url))
+    # report result status
+    status <- result$status_code
+    url <- result$url
+    
+    if(status != 200){
+      message(paste(Sys.time() |> format("%X"),
+                    "| Status", status,
+                    "| Failed URL:", url))
 
     # small pause to give user a chance to cancel
     Sys.sleep(6)
+    }
+    
+    if(status == 200){
+      metadata[[i]] <- fromJSON(rawToChar(result$content))
+    }
+
+    # EXTRACT THE MOST RECENT x-ratelimit-remaining and pause if it is 0
+    remaining <<- result$headers$`x-ratelimit-remaining` %>% as.numeric()
+    
+    if(remaining < 2){
+      
+      message(paste("|", Sys.time()|> format("%X"), "| Hit rate limit, will continue after one minute |", remaining, "remaining"))
+      
+      # ROTATE KEYS
+      api_keys <<- c(tail(api_keys, -1), head(api_keys, 1))
+      api_key <- api_keys[1]
+      #api_key <<- apikeys[runif(1, min=1, max=3.999) |> floor() ]
+      message(paste("Rotating api key to", api_key))
+      
+      Sys.sleep(60)
+    }
+    
+    if(metadata[[url]]$meta$lastPage == TRUE){break} # breaks loop when last page is reached
   }
-
-  # EXTRACT THE MOST RECENT x-ratelimit-remaining and pause if it is 0
-  remaining <<-  map(result, headers) |>
-    tail(1) |>
-    pluck(1, "x-ratelimit-remaining") |>
-    as.numeric()
-
-  if(remaining < 2){
-
-    message(paste("|", Sys.time()|> format("%X"), "| Hit rate limit, will continue after one minute |", remaining, "remaining"))
-
-    # ROTATE KEYS
-    api_keys <<- c(tail(api_keys, -1), head(api_keys, 1))
-    api_key <- api_keys[1]
-    #api_key <<- apikeys[runif(1, min=1, max=3.999) |> floor() ]
-    message(paste("Rotating api key to", api_key))
-
-    Sys.sleep(60)
-  }
-
-
-  # map the content of successful api results into a list
-  docket_metadata <- purrr::map_if(result, ~ status_code(.x) == 200, ~fromJSON(rawToChar(.x$content)))
 
   d <- map_dfr(docket_metadata, make_dataframe)
 
