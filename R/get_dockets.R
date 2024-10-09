ed71eccb259f9b91710c3c0919079cce523b1aac
mayakhuzam2 <you@example.com>
2024-10-09 11:26:42 -0500

    tempfile save edits, working on this still

--- R/get_dockets.R
+++ R/get_dockets.R
@@ -4,42 +4,49 @@
 
 get_dockets <- function(agency,
                         lastModifiedDate = Sys.time(),
-                        api_keys){
-
-  # Fetch the initial 5k and establish the base dataframe
-  metadata <- get_dockets_batch(agency, lastModifiedDate, api_keys)
-
-  # make an near-empty dataframe if the results are empty
-  if(nrow(metadata) == 0){
-    metadata <- tibble(lastpage = TRUE)
-  }
-
-  # Loop until last page is TRUE
-  while( !tail(metadata$lastpage, 1) | nrow(metadata) %% 5000 == 0 ) {
-
-    # Fetch the next batch of comments using the last modified date
-
-    nextbatch <- get_dockets_batch(agency,
-                                   lastModifiedDate = tail(metadata$lastModifiedDate, n = 1),
-                                   api_keys)
-
-    message(paste(nrow(metadata), "+", nrow(nextbatch)))
-
-    # Append next batch to comments
-    metadata <- suppressMessages(
-      bind_rows(metadata, nextbatch)
-    )
-
-    message(paste(" = ", nrow(metadata)))
-
-  }
-
+                        api_keys) {
+  
+  metadata_temp <- tempfile(fileext = ".rda")
+  
+  tryCatch({
+    # Fetch the initial 5k and establish the base dataframe
+    metadata <- get_dockets_batch(agency, lastModifiedDate, api_keys)
+    
+    # Make a near-empty dataframe if the results are empty
+    if (nrow(metadata) == 0) {
+      metadata <- tibble(lastpage = TRUE)
+    }
+    
+    # Loop until last page is TRUE
+    while (!tail(metadata$lastpage, 1) | nrow(metadata) %% 5000 == 0) {
+      # Fetch the next batch of comments using the last modified date
+      nextbatch <- get_dockets_batch(agency,
+                                     lastModifiedDate = tail(metadata$lastModifiedDate, n = 1),
+                                     api_keys)
+      
+      message(paste(nrow(metadata), "+", nrow(nextbatch)))
+      
+      # Append next batch to metadata
+      metadata <- suppressMessages(
+        bind_rows(metadata, nextbatch)
+      )
+      
+      message(paste(" = ", nrow(metadata)))
+    }
+    
+  }, error = function(e) {
+    message("An error occurred: ", e$message)
+    if (exists("metadata")) {
+      save(metadata, file = metadata_temp)
+      message("Docket data saved to: ", metadata_temp)
+    }
+  })
+  
+  # Return the metadata (no saving on normal completion)
   return(metadata)
-
 }
 
 # TESTING
 if(F){
 n <- get_dockets(agency = "NOAA", api_keys = api_keys)
 }
-
