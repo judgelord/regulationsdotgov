386cee2392f0d3bfdf03300b26a1e1ebb0e99ab5
judgelord <devin.jl@gmail.com>
2024-10-17 20:02:42 -0400

    fixes a bug with get_comments were documents with 0 comments were causing an error becasue metadata/content was null. #3 also fixes path formation so that 1. date formatting happens inside make_path 2. paths are formed one at a time allowing api keys to be rotated between pages #4 adds a while loop to keep trying a call that is not 200 #17

--- R/get_comments_batch.R
+++ R/get_comments_batch.R
@@ -7,36 +7,43 @@
 get_comments_batch <- function(commentOnId,
                                lastModifiedDate = Sys.time(),
                                api_keys){
-  
+
   api_key <- api_keys[1]
 
-  lastModifiedDate <- format_date(lastModifiedDate)
-
   # call the make path function to make paths for the first 20 pages of 250 results each
-  path <- make_path_commentOnId(commentOnId, lastModifiedDate, api_key)
-  
+  i <- 1
   metadata <- list()
-  
-  for (i in path){
-      
+
+  for (i in 1:20){
+
+    message(paste("Page", i))
+
+    path <- make_path_commentOnId(commentOnId,
+                                  lastModifiedDate,
+                                  page = i,
+                                  api_key)
+
     # map GET function over pages
-    result <- GET(i)
-    
+    result <- GET(path)
+
     # report result status
     status <- result$status_code
     url <- result$url
-    
-    if(status != 200){
+
+    while(status != 200){
       message(paste(Sys.time() |> format("%X"),
                     "| Status", status,
                     "| Failed URL:", url))
-      
+
       # small pause to give user a chance to cancel
-      Sys.sleep(6)
+      Sys.sleep(60)
+
+      result <- GET(path)
+
     }
-    
+
     ## print unsuccessful api calls (might be helpful to know which URLs are failing)
-    #I don't think we need this? Repetitive? 
+    #I don't think we need this? Repetitive?
     #purrr::walk2(result,
     #             path,
     #             function(response, url) {
@@ -48,32 +55,43 @@ get_comments_batch <- function(commentOnId,
     #               }
     #             }
     #)
-    
+
     if(status == 200){
-      metadata[[i]] <- fromJSON(rawToChar(result$content))
+      content <- fromJSON(rawToChar(result$content))
+      metadata[[i]] <- content
     }
-    
+
     # EXTRACT THE MOST RECENT x-ratelimit-remaining and pause if it is 0
     remaining <<- result$headers$`x-ratelimit-remaining` %>% as.numeric()
-    
+
     if(remaining < 2){
-      
-      message(paste("|", Sys.time()|> format("%X"), "| Hit rate limit, will continue after one minute |", remaining, "remaining"))
-      
+
+      message(paste("|", Sys.time()|> format("%X"), "| Hit rate limit |", remaining, "remaining"))
+
       # ROTATE KEYS
       api_keys <<- c(tail(api_keys, -1), head(api_keys, 1))
+      api_keys <- c(tail(api_keys, -1), head(api_keys, 1))
       api_key <- api_keys[1]
       #api_key <<- apikeys[runif(1, min=1, max=3.999) |> floor() ]
-      message(paste("Rotating api key to", api_key))
-      
-      Sys.sleep(60)
+      message(paste("Rotating to api key ending in", api_key |> str_replace(".{35}", "...")))
+
+      Sys.sleep(.60)
     }
-    
-    if(metadata[[url]]$meta$lastPage == TRUE){break} # breaks loop when last page is reached
+
+    content$meta$lastPage
+
+    if(!is.null(content$meta$lastPage)){
+      if(content$meta$lastPage == TRUE){break} # breaks loop when last page is reached
+    }
   }
 
   # map the list into a dataframe with the information we care about
   d <- map_dfr(metadata, make_dataframe)
+
+  # if there was none, make an empty dataframe
+  if(nrow(d)==0){
+    d <- tibble(lastpage = TRUE)
+  }
 
   # add back in the id for the document being commented on
   d$commentOnId <- commentOnId
--- R/make_path_commentOnId.R
+++ R/make_path_commentOnId.R
@@ -1,16 +1,17 @@
 #' @keywords internal
 
-make_path_commentOnId <- function(commentOnId, 
-                                  lastModifiedDate, 
+make_path_commentOnId <- function(commentOnId,
+                                  lastModifiedDate,
+                                  page,
                                   api_key){
   paste0("https://api.regulations.gov",
          "/v4/",
          "comments",
          "?",
          "filter[commentOnId]=", commentOnId, "&",
-         "filter[lastModifiedDate][le]=", lastModifiedDate, "&", #less than or equal to (vs [ge] in the api docs)
+         "filter[lastModifiedDate][le]=", format_date(lastModifiedDate), "&", #less than or equal to (vs [ge] in the api docs)
          "page[size]=250", "&",
-         "page[number]=", 1:20, "&", #FIXME replace with 2 with 20 when done testing
+         "page[number]=", page, "&", #FIXME replace with 2 with 20 when done testing
          "sort=-lastModifiedDate,documentId", "&",
          "api_key=", api_key)
 }
