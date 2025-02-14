18302e11a4d51360472091b5985f89f65175dac8
judgelord <devin.jl@gmail.com>
2025-02-14 11:02:08 -0500

    capping get_commentsOnId at 2m to deal with infinite loops

--- R/get_commentsOnId.R
+++ R/get_commentsOnId.R
@@ -18,7 +18,7 @@ get_commentsOnId <- function(objectId,
 
     # Loop until last page is TRUE
 
-    while( !tail(metadata$lastpage, 1) | nrow(metadata) %% 5000 == 0 ) {
+    while( nrow(metadata) < 2000000 & (!tail(metadata$lastpage, 1) | nrow(metadata) %% 5000 == 0) ) {
 
     # Fetch the next batch of comments using the last modified date
     nextbatch <- get_comments_batch(objectId,
