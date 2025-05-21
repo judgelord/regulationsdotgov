8f68479028d00978e589bedd213f6868fb88e3b5
mayakhuzam2 <you@example.com>
2025-05-21 12:08:04 -0500

    #30 better error handling for api limits on get_dockets

--- R/get_dockets_batch.R
+++ R/get_dockets_batch.R
@@ -11,6 +11,7 @@ get_dockets_batch <- function(agency,
   api_key <- sample(api_keys, 1)
   
   metadata <- list()
+  failed_paths <- list()
 
   for (i in 1:20) {
     message(paste("Page", i))
@@ -25,46 +26,78 @@ get_dockets_batch <- function(agency,
     result <- httr::GET(path)
     status <- result$status_code
     url <- result$url
+    remaining <- as.numeric(result$headers$`x-ratelimit-remaining`)
 
-    if (status != 200) {
+    
+    if(status != 200 & status != 429){
       message(paste(Sys.time() |> format("%X"),
                     "| Status", status,
                     "| Failed URL:", path |> stringr::str_replace("&api_key=.+(.{4})", "&api_key=XXX\\1")))
-      Sys.sleep(60)  # Pause before retrying -
-      #TODO retry path that failed
+      
+      failed_paths[[i]] <- path
+    
+      # small pause to give user a chance to cancel
+      Sys.sleep(6)
     }
-
+    
+    #if previously failed due to rate limit, try again
+    while(status == 429){
+      
+      message(paste(Sys.time() |> format("%X"),
+                    "| Status", status,
+                    "| Failed URL:", path |> stringr::str_replace("&api_key=.+(.{4})", "&api_key=XXX\\1")))
+      
+      message(paste("|", Sys.time()|> format("%X"),
+                    "| Hit rate limit |",
+                    remaining, "remaining | api key ending in",
+                    api_key |> stringr::str_replace(".+(.{4})", "XXX\\1")))
+      
+      # ROTATE KEYS
+      api_key <- sample(api_keys, 1)
+      message(paste("Rotating to api key ending in", api_key |> stringr::str_replace(".+(.{4})", "XXX\\1")))
+      
+      # try again
+      path <- make_path_dockets(agency, 
+                                lastModifiedDate, 
+                                lastModifiedDate_mod =  lastModifiedDate_mod,
+                                page = i, 
+                                api_key = api_key)
+      
+      result <- httr::GET(path)
+      
+      status <- result$status_code
+      
+      remaining <- result$headers$`x-ratelimit-remaining` |> as.numeric()
+      
+      message(paste("|", "Trying:", agency ,"again",
+                    "| now:", status,
+                    "| limit-remaining", remaining, "|"))
+      
+      # pause to reset rate limit
+      if(status == 429 | remaining < 2){
+        message(paste(Sys.time()|> format("%X"), "- Hit rate limit again, will continue after one minute"))
+        Sys.sleep(60)
+      }
+    }
+    
     # Parse and store the content if the status is 200
     if(status == 200){
       content <- jsonlite::fromJSON(rawToChar(result$content))
       metadata[[i]] <- content
     }
 
-    # Extract rate limit and pause if necessary
-    remaining <- as.numeric(result$headers$`x-ratelimit-remaining`)
-
-    if(remaining < 2){
-
-      message(paste("|", Sys.time()|> format("%X"),
-                    "| Hit rate limit |",
-                    remaining, "remaining | api key ending in",
-                    api_key |> stringr::str_replace(".{35}", "...")))
-
-      # ROTATE KEYS
-      api_key <- sample(api_keys, 1)
-      message(paste("Rotating to api key ending in", api_key |> stringr::str_replace(".{35}", "...")))
-
-      Sys.sleep(.60)
-    }
-
     # Break loop if the last page is reached
     if(!is.null(content$meta$lastPage)){
       if(content$meta$lastPage == TRUE){break} # breaks loop when last page is reached
     }
-
+    
   }
 
   d <- purrr::map_dfr(metadata, make_dataframe)
+  
+  if(length(failed_paths > 0)){
+    message("Failed paths:", failed_paths)
+  }
 
   #if(nrow(d)==0){
   #  d <- tibble::tibble(lastpage = TRUE)}
